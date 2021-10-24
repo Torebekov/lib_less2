a34c792d730dddbc3985a323723a88c93bc1addc
Arman Torebekov <torebek.arman14@gmail.com>
2021-10-24 23:23:43 +0600

    v1.0.1

--- libs.go
+++ libs.go
@@ -1,14 +1,23 @@
 package lib_less2
 
-func ChangeCase(s string) string {
-	res := []rune(s)
-	for i, v := range res {
-		switch {
-		case v >= 'a' && v <= 'z':
-			res[i] -= 32
-		case v >= 'A' && v <= 'Z':
-			res[i] += 32
-		}
+import (
+	"fmt"
+	"math"
+)
+
+func FindRoots(a, b, c float64) []string {
+	d := b*b - 4*a*c
+	var res []string
+	switch {
+	case d > 0:
+		x1 := (-b + math.Sqrt(d)) / (2 * a)
+		x2 := (-b - math.Sqrt(d)) / (2 * a)
+		res = append(res, fmt.Sprintf("%v", x1), fmt.Sprintf("%v", x2))
+	case d == 0:
+		x := -b / (2 * a)
+		res = append(res, fmt.Sprintf("%v", x))
+	case d < 0:
+		res = append(res, "No real roots")
 	}
-	return string(res)
+	return res
 }
